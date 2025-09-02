93e1b22419808a810dbef1b850122a50e9219014
LaitEntier <axelzinc@hotmail.fr>
2025-09-02 17:16:02 +0200

    refactor: replace app.run with server.run in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,6 @@
-import sys
-import os
-
 from app import app
 
-app.config.suppress_callback_exceptions = True
+server = app.server
 
 if __name__ == "__main__":
-    app.run(
-        host='0.0.0.0', 
-        port=8000,
-        debug=True
-    )
+    server.run(host='0.0.0.0', port=8000)
